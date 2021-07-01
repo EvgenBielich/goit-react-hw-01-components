6211d54c717c93258b9bc13b10cdb6ee7b814aef
Evgen Bielich <belichevgenij@gmail.com>
2021-07-01 03:08:03 +0300

    profile

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Profile from "./components/Profile/Profile";
+
+import user from "./components/Profile/user.json";
+
+export default function App() {
+  return (
+    <>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      ,
+    </>
+  );
+}
