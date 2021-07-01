3b6ac83832a190bd943774fcd5da9086ac1422c8
Evgen Bielich <belichevgenij@gmail.com>
2021-07-01 14:23:44 +0300

    TransactionHistory

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Profile from "./components/Profile/Profile";
 import Statistics from "./components/Statistics/Statistics";
 import FriendList from "./components/FriendList/FriendList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 
 import user from "./components/Profile/user.json";
 import statisticalData from "./components/Statistics/statistical-data.json";
 import friends from "./components/FriendList/friends.json";
+import transactions from "./components/TransactionHistory/transactions.json";
 
 export default function App() {
   return (
@@ -17,8 +19,9 @@ export default function App() {
         stats={user.stats}
       />
       ,
-      <Statistics title="Upload stats" stats={statisticalData} />;
+      <Statistics title="Upload stats" stats={statisticalData} />,
       <FriendList friends={friends} />,
+      <TransactionHistory items={transactions} />;
     </>
   );
 }
